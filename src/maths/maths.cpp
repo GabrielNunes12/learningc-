c8511e22ccec2dd608d55767c456e50420920f5f
Gabriel <prismaticnetwork12@gmail.com>
2023-11-16 18:09:52 -0300

    feat: learning c++ math functions

---
+++ src/maths/maths.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Gabriel Nunes on 11/16/2023.
+//
+
+#include <cmath>
+#include <iostream>
+int main() {
+    std::max(1,4); //will return 4;
+    std::min(0,2); // will return 0;
+    //cmath functions (https://cplusplus.com/reference/cmath/)
+    pow(2,3);
+    sqrt(9);
+    abs(-44);
+    round(3.11);
+    ceil(4.20);
+    floor(3.99);
+    return 0;
+}
